75855b9be903ae3436a2a0151ae050cc03e38ae1
adelacruz <76147683@continental.edu.pe>
2026-01-19 18:25:09 -0500

    Se agrego el test conjunto un elemento

--- tests/TestConjunto.py
+++ tests/TestConjunto.py
@@ -1,7 +1,12 @@
 import unittest
 from src.logica.Conjunto import Conjunto
 
+
 class TestConjunto(unittest.TestCase):
     def test_conjunto_vacio_retornaNone(self):
-        Conjunto = Conjunto
-        self.assertIsNone(Conjunto.promedio())
+        conjunto = Conjunto([])
+        self.assertIsNone(conjunto.promedio())
+
+    def test_conjunto_unitario_retornableUnicoElemento(self):
+        conjunto = Conjunto([5])
+        self.assertEqual(5, conjunto.promedio())
