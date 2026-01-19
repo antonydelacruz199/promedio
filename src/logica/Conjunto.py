c822986cff56697e9299e4570007dffe66facdeb
adelacruz <76147683@continental.edu.pe>
2026-01-19 18:39:30 -0500

    Service yltima version

--- src/logica/Conjunto.py
+++ src/logica/Conjunto.py
@@ -5,6 +5,10 @@ class Conjunto:
     def promedio(self):
         if len(self.__conjunto) == 1:
             return (self.__conjunto[0])
+        elif len(self.__conjunto) == 2:
+            return (self.__conjunto[0]+self.__conjunto[1])/2
+        elif len(self.__conjunto) > 2:
+            return sum(self.__conjunto)/len(self.__conjunto)
         else:
             return None
     
--- tests/TestConjunto.py
+++ tests/TestConjunto.py
@@ -10,3 +10,11 @@ class TestConjunto(unittest.TestCase):
     def test_conjunto_unitario_retornableUnicoElemento(self):
         conjunto = Conjunto([5])
         self.assertEqual(5, conjunto.promedio())
+    
+    def test_conjunto_dosElementos_retornaPromedioElementos(self):
+        conjunto = Conjunto([5, 7])
+        self.assertEqual(6, conjunto.promedio())
+    
+    def test_conjunto_nElementos_retornaPromedioNElementos(self):
+        conjunto = Conjunto([2,4,5,6,7,8])
+        self.assertEqual()
